7c100efe2b806ce6e79c31ebf87e2fea5a6bfba6
jamesjbot <jongs.j@gmail.com>
2020-11-28 12:01:27 -0500

    feat: Added regular expression check for valid website.

--- src/client/js/formHandler.js
+++ src/client/js/formHandler.js
@@ -4,9 +4,25 @@
 async function handleSubmit(event) {
     event.preventDefault();
     let formText = document.getElementById('searchValue').value;
-    await Client.getSentiment(formText).then(message => {
-      Client.manipulateDomWithJSONAtKey(document, message,'results');
-    });
+
+    let validWebsiteAddress = false;
+    let regularExpression = new RegExp('^https?:\/\/[a-zA-Z]{3,}\.[a-zA-Z]{1,}\.[a-zA-Z]{3,}');
+    validWebsiteAddress = regularExpression.test(formText);
+
+    if (validWebsiteAddress) {
+      // Notify the user that their request was submitted
+      document.getElementById('results').textContent = 'Processing...';
+
+      // Submit request to server
+      await Client.getSentiment(formText).then(message => {
+        Client.manipulateDomWithJSONAtKey(document, message,'results');
+      });
+
+    } else {
+
+    document.getElementById('results').textContent = 'You did not enter a website starting with http:// or https://';
+
+    }
 }
 
 export { handleSubmit };
