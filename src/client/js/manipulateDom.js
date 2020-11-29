b5e3593795f9285374072c695c95a9f26309b720
jamesjbot <jongs.j@gmail.com>
2020-11-28 20:55:52 -0500

    feat: If the api request failed for any reason thell the user could not analyze website

--- src/client/js/manipulateDom.js
+++ src/client/js/manipulateDom.js
@@ -2,17 +2,25 @@
 /*jshint esversion:8*/
 function manipulateDomWithJSONAtKey(document, JSON, key) {
 
-    console.log('mani received', JSON);
+    if (JSON.status.code == '0') { // Success!
 
-    let element = document.getElementById(key);
+      let element = document.getElementById(key);
 
-    element.innerHTML =
-    `<p>Aggrement: ${JSON.agreement}</p>`
-    + `<p>Subjectivity: ${JSON.subjectivity}</p>`
-    + `<p>Confidence: ${JSON.confidence}</p>`
-    + `<p>Irony: ${JSON.irony}</p>`;
+      element.innerHTML =
+      `<p>Aggrement: ${JSON.agreement}</p>`
+      + `<p>Subjectivity: ${JSON.subjectivity}</p>`
+      + `<p>Confidence: ${JSON.confidence}</p>`
+      + `<p>Irony: ${JSON.irony}</p>`;
 
-    return document;
+      return document;
+
+    } else {
+
+      document.getElementById(key).textContent = 'Could not analyze website'
+
+    }
+
+
 }
 
 export { manipulateDomWithJSONAtKey };
