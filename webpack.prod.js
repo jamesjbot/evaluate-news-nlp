a2a5b6ec339b40bf8c1ee9da386e3bbe4eb2c1a2
jamesjbot <jongs.j@gmail.com>
2020-12-27 16:00:21 -0500

    feat: Add Workbox and minicss

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,15 +1,20 @@
 const path = require('path')
 const webpack = require('webpack')
 const HtmlWebPackPlugin = require("html-webpack-plugin")
+const WorkboxPlugin = require('workbox-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 module.exports = {
     entry: './src/client/index.js',
-    //mode: 'production', TODO reinstate this
+    mode: 'production',
     mode: 'development',
-    //output: { TODO reinstate this
-    //    libraryTarget: 'var',
-    //    library: 'Client'
-    //},
+    output: {
+       libraryTarget: 'var',
+       library: 'Client'
+    },
     module: {
         rules: [
             {
@@ -19,14 +24,28 @@ module.exports = {
             },
             {
                 test: /\.scss$/,
-                use: [ 'style-loader', 'css-loader', 'sass-loader' ]
+                use: [ MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader' ]
             }
         ]
     },
+    optimization: {
+      minimizer: [new TerserPlugin({}), new OptimizeCSSAssetsPlugin({})],
+    },
     plugins: [
+	new MiniCssExtractPlugin(),
         new HtmlWebPackPlugin({
             template: "./src/client/views/index.html",
             filename: "./index.html",
-        })
+        }),
+        new CleanWebpackPlugin({
+            // Simulate the removal of files
+            dry: true,
+            // Write Logs to Console
+            verbose: true,
+            // Automatically remove all unused webpack assets on rebuild
+            cleanStaleWebpackAssets: true,
+            protectWebpackAssets: false
+        }),
+        new WorkboxPlugin.GenerateSW()
     ]
 }
