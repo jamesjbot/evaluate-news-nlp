937df248cd37e60311250b7bc3c6e096915df41f
jamesjbot <jongs.j@gmail.com>
2020-12-27 19:38:22 -0500

    chore: Remove debugging code

--- src/server/index.js
+++ src/server/index.js
@@ -5,13 +5,13 @@
 const dotEnv = require('dotenv'); // Used to get apikey from the environment
 dotEnv.config();
 
-var path = require('path')
-const express = require('express')
-const mockAPIResponse = require('./mockAPI.js')
+var path = require('path');
+const express = require('express');
+const mockAPIResponse = require('./mockAPI.js');
 const fetch = require("node-fetch");
 const cors = require('cors');
-const meaningCloudURL = 'https://api.meaningcloud.com/sentiment-2.1?key='
-const app = express()
+const meaningCloudURL = 'https://api.meaningcloud.com/sentiment-2.1?key=';
+const app = express();
 
 app.use(cors());
 app.use(express.static('dist')); // use for enrivronment api key
@@ -19,32 +19,15 @@ app.use(express.static('dist')); // use for enrivronment api key
 //Here we are configuring express
 app.use(express.json()); // Enable the json parser from express.
 
-// TODO: Temporarily remove the abiliity to server the webpage as
-// web dev server is doing that.
-// app.get('/', function (req, res) {
-//     // res.sendFile('dist/index.html')
-//     res.sendFile(path.resolve('src/client/views/index.html'))
-// })
-
 // designates what port the app will listen to for incoming requests
 app.listen(7777, function () {
-    console.log('Example app listening on port 7777!')
-})
+    console.log('App listening on port 7777!');
+});
 
-app.get('/test', function (req, res) {
-    //console.log('Receved Request for /test')
-    //console.log('The request is:');
-    console.log(req.body)
-    meaningCloudAPIFetch(req)
-        .then(response => response)
-        .then(data => data.json())
-        .then(jsonData => {
-          console.log('Agreement:'+jsonData.agreement);
-          console.log('Subjectivity:'+jsonData.subjective);
-          console.log('Confidence:'+jsonData.confidence);
-          res.send(jsonData); // Returning JSONData
-        });
-    //res.send(req.body);
+// Return home webpage
+app.get('/', function (req, res) {
+    // res.sendFile('dist/index.html')
+    res.sendFile(path.resolve('src/client/views/index.html'));
 });
 
 // POST methods to receive sentiment analysis
@@ -59,16 +42,6 @@ app.post('/sentiment', function (req, res) {
         console.log('Confidence:'+jsonData.confidence);
         res.send(jsonData); // Returning JSONData
       });
-
-  // Remove code below
-  // try {
-  //   let data = req.body; //By having express.json(), this is automatically parsed
-  //   console.log('received:',data.data);
-  // } catch (err){
-  //   console.log('error parsing request body:', err);
-  // }
-  // console.log('past body.json()');
-  // res.send(JSON.stringify(responseJson));
 });
 
 function meaningCloudAPIFetch(input) {
@@ -82,12 +55,3 @@ function meaningCloudAPIFetch(input) {
     return fetch(address);
 
 }
-
-
-const responseJson = {
-	status: {code:'0',msg: "ok"},
-	agreement: 'AGREEMENT',
-	subjectivity: 'OBJECTIVE',
-	confidence: '100',
-	irony: 'NONIRONIC'
-};
